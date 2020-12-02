5cceb7d0b9369b329b4beb16fea7a8d34df062cc
Uwe Salomon <post@uwesalomon.de>
2020-12-02 22:07:24 +0100

    support hexadecimal cRGB fill colors

--- src/hints.cpp
+++ src/hints.cpp
@@ -24,7 +24,7 @@
 
 Hints Hints::from(const TextImg& txt, Graph& graph)
 {
-  static QRegExp colorRegex{R"(\bc(?:[0-9]{3}|[A-Z]{3})\b)"};
+  static QRegExp colorRegex{R"(\bc[0-9A-Z]{3}\b)"};
 
   Hints result;
   for (int y = 0; y < txt.height(); ++y)
@@ -33,24 +33,19 @@ Hints Hints::from(const TextImg& txt, Graph& graph)
     for (int idx = 0; (idx = colorRegex.indexIn(line, idx)) != -1; idx += colorRegex.matchedLength())
     {
       QColor color;
-      if (line[idx+1].isDigit())
-      {
-        color.setRed(line[idx+1].digitValue() * 255 / 9);
-        color.setGreen(line[idx+2].digitValue() * 255 / 9);
-        color.setBlue(line[idx+3].digitValue() * 255 / 9);
-      }
+      auto name = line.midRef(idx+1, 3);
+
+      if      (name == "RED") color = QColor("#FF4136");
+      else if (name == "GRE") color = QColor("#2ECC40");
+      else if (name == "BLU") color = QColor("#0074d9");
+      else if (name == "PNK") color = QColor("#BC35CF");
+      else if (name == "YEL") color = QColor("#FFDC00");
+      else if (name == "BLK") color = QColor("#111111");
       else
-      {
-        auto name = line.midRef(idx+1, 3);
-        if      (name == "RED") color = QColor("#FF4136");
-        else if (name == "GRE") color = QColor("#2ECC40");
-        else if (name == "BLU") color = QColor("#0074d9");
-        else if (name == "PNK") color = QColor("#BC35CF");//"#B10DC9");
-        else if (name == "YEL") color = QColor("#FFDC00");
-        else if (name == "BLK") color = QColor("#111111");
-        else
-          continue;
-      }
+        color = QColor("#" + name);
+
+      if (!color.isValid())
+        continue;
 
       result.emplace_back(idx, y, color);
       graph.setEmpty(idx, y, colorRegex.matchedLength());
